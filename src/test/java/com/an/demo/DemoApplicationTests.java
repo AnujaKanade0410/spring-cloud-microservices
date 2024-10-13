5dc1f4bd5d8e9db02705f61bdf9fc0ae6c890612
AnujaKanade0410 <anuj.itworld@gmail.com>
2024-10-13 15:02:47 +0200

    Application is up and running -step 1

--- src/test/java/com/an/demo/DemoApplicationTests.java
+++ src/test/java/com/an/demo/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.an.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "debug=true")
 class DemoApplicationTests {
 
 	@Test
